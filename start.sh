d48108f5eaa83bb5e66e488d88cf0d466d6bb04b
zayntype <85392583+zayntype@users.noreply.github.com>
2021-08-18 14:49:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -34,7 +34,7 @@ if [[ -n $MAX_CONCURRENT_DOWNLOADS ]]; then
 fi
 
 sed -i'' -e "/bt-tracker=/d" $(pwd)/aria.conf
-tracker_list=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',' | cat`
+tracker_list=`curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat`
 echo -e "bt-tracker=$tracker_list" >> $(pwd)/aria.conf
 
 # Remove the .bak file got created from above sed
